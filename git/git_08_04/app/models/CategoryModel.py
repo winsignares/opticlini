729da94fa81d2bb7f8a40bc510f8f3a5329112e1
Leisyzo <Leisya-zarateo@unilibre.edu.co>
2025-04-08 16:05:22 -0500

    Add files via upload

---
+++ git/git_08_04/app/models/CategoryModel.py
@@ -0,0 +1,17 @@
+from config import app, db, ma
+
+class Category(db.Model):
+    __tablename__ = 'tblcategory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    namecategory = db.Column(db.String(100))
+
+    def __init__(self, namecategory):
+        self.namecategory = namecategory
+
+with app.app_context():
+    db.create_all()
+
+class CategorySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'namecategory')
