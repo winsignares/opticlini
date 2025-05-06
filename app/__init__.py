f73782837a8fa306718306742ce0e31e28b73f5b
Santyzz0311 <santyg0311@gmail.com>
2025-05-05 23:13:48 -0500

    Add: vistas y recursos estáticos de la página

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,16 @@ def create_app():
     def index():
         return render_template("index.html")
 
+    @app.route('/cart')
+    def cart():
+        return render_template("cart.html")
+
+    @app.route('/about')
+    def about():
+        return render_template("about.html")
+
+    @app.route('/pay-cart')
+    def pay_cart():
+        return render_template("pay-cart.html")
+
     return app
